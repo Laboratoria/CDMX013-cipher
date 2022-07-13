336386c60c6f68e9a1d7a68621b2747fa7fe7a3b
menamilochomil <jimenamilochomil@gmail.com>
2022-07-12 22:39:48 -0500

    Primera propuesta de imagen y primera función

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+function showCipherBox() {
+    if (document.getElementById("buttonOne").checked) {
+        document.getElementById("rootSecondView").style.display= "block";
+        document.getElementById("rootFirstView").style.display= "none";
+    }
+    //let secondView=document.getElementById("rootSecondView");
+    //let disappearFirst=document.getElementById("rootFirstView");
+    //disappearFirst.style.display="none";
+    //secondView.style.display="block";
+      }
