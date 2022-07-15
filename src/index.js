052d86b027c3363821e8f1acae44e5a4dbc4b596
joyceams <joyceaketzalli@gmail.com>
2022-07-15 12:08:14 -0500

    third version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+let resultados = document.getElementById("codificar");
+/*
+addEdentListener es un método que ejecuta la acción que tiene que pasar cuando interactúen con lo que va antes del punto, después, ejecuta la función a su derecha
+*/
+
+resultados.addEventListener("click", saludo)
+
+function saludo (){
+    alert("Hola, cosa");
+}
