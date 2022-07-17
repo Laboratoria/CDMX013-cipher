0099841ee6630881400e17755de123c74b8863fd
Arely Gutiérrez <yleraycnan@hotmail.com>
2022-07-16 23:54:02 -0500

    pantalla cipher con formato

--- src/cipher.js
+++ src/cipher.js
@@ -3,3 +3,5 @@ const cipher = {
 };
 
 export default cipher;
+//lo anterior ya venía en el código, quiza sea para probar el testgxz
+//funcion 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-
+cipherTEXT(){
+    
+    console.log("esto va bien")
+}
