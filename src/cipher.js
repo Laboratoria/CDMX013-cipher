03a156cf50872858128c3c842c7f0548e5b42ac8
Arely Gutiérrez <yleraycnan@hotmail.com>
2022-07-18 19:36:46 -0500

    noRefreshData

--- src/cipher.js
+++ src/cipher.js
@@ -3,5 +3,4 @@ const cipher = {
 };
 
 export default cipher;
-//lo anterior ya venía en el código, quiza sea para probar el testgxz
-//funcion 
+////////////////////////
--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-cipherTEXT(){
-    
-    console.log("esto va bien")
-}
+//cipherTEXT(   ){
+  //  event.preventDefault() // Previene recarga del navegador
+    //console.log("esto va bien");
+//}
+
+
+// const variable que no se reasigna o /// let variable que se vuelve a reasignar 
+let GetCipherText=document.getElementById("cipherFunction"); 
+
+GetCipherText.addEventListener("click", function (event){
+    event.preventDefault() // Previene recarga del navegador
+    alert("esto va bien");
+});
