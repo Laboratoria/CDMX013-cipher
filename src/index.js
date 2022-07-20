5887c0260cfcccd8b9664eecd2a73c4d8f74388d
joyceams <joyceaketzalli@gmail.com>
2022-07-19 19:08:37 -0500

    nuevo cambio

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
-let resultados = document.getElementById("codificar");
+
+let entrada = document.getElementById("btncodif");
 /*
-addEdentListener es un método que ejecuta la acción que tiene que pasar cuando interactúen con lo que va antes del punto, después, ejecuta la función a su derecha
+addEventListener es un método que ejecuta la acción que tiene que pasar cuando interactúen con lo que va antes del punto, después, ejecuta la función a su derecha
 */
 
-resultados.addEventListener("click", saludo)
+entrada.addEventListener("click", saludo)
 
 function saludo (){
-    alert("Hola, cosa");
+    alert("Hola, vamos a comenzar");
 }
+
+let resultado = document.getElementById("btndecodif");
+resultado.addEventListener("click", despedida)
+
+function despedida (){
+    alert("Tu mensajes está listo");
+}
+
+cadena.toUpperCase() /*sirve para que las letras que se introduzcan se conviertan en mayúsculas*/
+charCode() fromCharCode() /*para convertir las letras a ASCII y viceversa*/
