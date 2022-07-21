bff55828039bbdb9c729d48c952a533d7405302c
Arely Gutiérrez <yleraycnan@hotmail.com>
2022-07-21 15:45:11 -0500

    objetoConcreto

--- src/cipher.js
+++ src/cipher.js
@@ -1,29 +1,28 @@
-const cipher = {
-    /////////////////////////
-    
-   // function encode(offset, string){
 
-     //   let m = '';
-       // for(let i=0; i=string.length; i++){
+
+const cipher = {
+
+     encode: function (offset, string) {
+        
+        let m = '';
+        for(let i=0; i<=string.length; i++){
           
-         //   SToN +='string'.charCodeAt[i];//string to number
-            //Convierte a numero y hago operaciones para cifrado
-           // let j += (string[i]-65+offset)%26+65;
-            //devuelve a ASCII
-            
-          //  m += l.fromCharCode[i];
-          //  console.log(document.getElementById("m").value);
-       // }
-       // document.getElementById("m").innerHTML = "Tu mensaje cifrado es: " + m;  
-    //}
-   
+            let SToN ='string'.charCodeAt[i];//string to number
+            let j = (SToN[i]-65+offset)%26 + 65; //Convierte a numero y hago operaciones para cifrado
+            m = j.fromCharCode[i]; //devuelve a ASCII
+            //console.log(document.getElementById("m").value);
+        }
+        //document.getElementById("m").innerHTML = "Tu mensaje cifrado es: " + m;  
+        return m;
+    }
 };
 /////metodos cipher.enconde y cipher.decode/
 //cipher.encode();
+//cipher.encode();
+
+
 
 export default cipher;
 ////////////////////////
 
-
-//cipher.decode(offset, string)
 
