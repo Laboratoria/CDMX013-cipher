d15c9491b80c7d50bcb8df1c31cad0f96fec4b9e
Arely Gutiérrez <yleraycnan@hotmail.com>
2022-07-22 18:45:35 -0500

    cifrado listo

--- src/cipher.js
+++ src/cipher.js
@@ -9,13 +9,13 @@ const cipher = {
         let SToN="";
         let j="";
         
-        for(let i=0; i<=string.length; i++){
+        for(let i=0; i<string.length; i++){
             //console.log(string.charCodeAt(i));
             SToN =string.charCodeAt(i);//string to number
             console.log(SToN);
-             j = (SToN[i]-65 + offset) % 26 + 65; //Convierte a numero y hago operaciones para cifrado
+             j = (SToN-65 + offset) % 26 + 65; //Convierte a numero y hago operaciones para cifrado
              console.log(j);
-             m = String.fromCharCode(j); //devuelve a ASCII
+             m += String.fromCharCode(j); //devuelve a ASCII
              console.log(m)
             //console.log(document.getElementById("m").value);*/
         }
