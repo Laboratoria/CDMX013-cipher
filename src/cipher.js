cb124894502a1b1e91d51e98f6b539b4f683a626
Arely Gutiérrez <yleraycnan@hotmail.com>
2022-07-25 14:36:45 -0500

    Descifrado Listo

--- src/cipher.js
+++ src/cipher.js
@@ -12,12 +12,13 @@ const cipher = {
         for(let i=0; i<string.length; i++){
             //console.log(string.charCodeAt(i));
             SToN =string.charCodeAt(i);//string to number
-            console.log(SToN);
-             j = (SToN-65 + offset) % 26 + 65; //Convierte a numero y hago operaciones para cifrado
+            //console.log(SToN);
+             j = Math.abs(SToN - 65 + offset) % 26 + 65; //Convierte a numero y hago operaciones para cifrado
+             console.log(Math.abs(SToN))
              console.log(j);
              m += String.fromCharCode(j); //devuelve a ASCII
-             console.log(m)
-            //console.log(document.getElementById("m").value);*/
+             //console.log(m)
+            
         }
         //document.getElementById("m").innerHTML = "Tu mensaje cifrado es: " + m;  
         return m;
@@ -31,14 +32,29 @@ const cipher = {
       let j="";
       
       for(let i=0; i<string.length; i++){
-          //console.log(string.charCodeAt(i));
+          console.log(string.charCodeAt(i));
           SToN =string.charCodeAt(i);//string to number
           console.log(SToN);
-           j = (SToN -65 - offset ) % 26 + 65; //Convierte a numero y hago operaciones para cifrado
-           console.log(j);
-           m += String.fromCharCode(j); //devuelve a ASCII
-           console.log(m)
-          //console.log(document.getElementById("m").value);*/
+
+          
+              let g = (SToN -65 - offset) 
+              if(g<0){
+
+                j = 91 + (g % 26);
+              
+                m += String.fromCharCode(j); //devuelve a ASCII
+                console.log(m)
+
+              }else{
+
+              j = (SToN -65 - offset) % 26 + 65; //Convierte a numero y hago operaciones para cifrado
+              //console.log(SToN -65 - offset );
+              //console.log(SToN -65 - offset )%26;
+              //console.log(j);
+              m += String.fromCharCode(j); //devuelve a ASCII
+              console.log(m)
+              //console.log(document.getElementById("m").value);*/
+              }  
       }
       //document.getElementById("m").innerHTML = "Tu mensaje cifrado es: " + m;  
       return m;
