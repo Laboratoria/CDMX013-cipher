b46ace96bb08d2f85917c9745bf0db84ca6c78e5
Arely Gutiérrez <yleraycnan@hotmail.com>
2022-07-25 11:23:37 -0500

    DecodeMAYUSCULAS

--- src/cipher.js
+++ src/cipher.js
@@ -21,7 +21,28 @@ const cipher = {
         }
         //document.getElementById("m").innerHTML = "Tu mensaje cifrado es: " + m;  
         return m;
-    }
+    },
+
+    decode: function (offset, string) {
+      //console.log(offset)
+      //console.log(string)
+    let m = '';
+      let SToN="";
+      let j="";
+      
+      for(let i=0; i<string.length; i++){
+          //console.log(string.charCodeAt(i));
+          SToN =string.charCodeAt(i);//string to number
+          console.log(SToN);
+           j = (SToN -65 - offset ) % 26 + 65; //Convierte a numero y hago operaciones para cifrado
+           console.log(j);
+           m += String.fromCharCode(j); //devuelve a ASCII
+           console.log(m)
+          //console.log(document.getElementById("m").value);*/
+      }
+      //document.getElementById("m").innerHTML = "Tu mensaje cifrado es: " + m;  
+      return m;
+  }
 };
 /////metodos cipher.enconde y cipher.decode/
 //cipher.encode();
--- src/index.js
+++ src/index.js
@@ -1,28 +1,52 @@
 import cipher from './cipher.js';
 
-//console.log(cipher);
-//cipherTEXT(){
- //   event.preventDefault() // Previene recarga del navegador
- //   console.log("esto va bien")
-//}
-
 // const variable que no se reasigna o /// let variable que se vuelve a reasignar 
-let GetCipherText=document.getElementById("cipherFunction"); 
-let string = document.getElementById("message");
+let getCipherText=document.getElementById("cipherFunction"); //id está en el  botón cifrar
+let string = document.getElementById("message");// id está en el texto de entrada
 let offset = document.getElementById("offsetID");
 
-GetCipherText.addEventListener("click", function (event){
-    event.preventDefault() // Previene recarga del navegador
-    //alert("esto va bien");
-    //console.log(document.getElementById("message").value);
-    //console.log(document.getElementById("offsetID").value);
-  
-   
-    // alert(cipher.encode(offset, string));
-    //cipher.encode(offset, string);
-    //document.getElementById("m").innerHTML = "Tu mensaje cifrado es: " + m;
-   document.getElementById("cipherText").innerHTML = "Tu mensaje cifrado es: " + cipher.encode(Number(offset.value),string.value);
+let getCipherText2 =document.getElementById("cipherFunction2"); //id está en botón descifrar
+//let string2 = document.getElementById("message2"); // id está en el texto de entrada
+//let offset2 = document.getElementById("offsetID2");// id enté en el dato que ingresa offset
+
     
-});
+    if(getCipherText === null){   
+        //hacemos lo que se requiera en caso de que la variable contenga null
+        //si getChipherText es null, es porque NO estamos en cifrar sino en DESCIFRAR  
+        getCipherText2.addEventListener("click", function (event){
+            event.preventDefault(); // Previene recarga del navegador
+            //alert("esto va bien");
+            //console.log(document.getElementById("message").value);
+            //console.log(document.getElementById("offsetID").value);
+            // alert(cipher.encode(offset2, string2));
+            //cipher.encode(offset, string);
+           // //document.getElementById("m").innerHTML = "Tu mensaje cifrado es: " + m;
+           document.getElementById("decipherText").innerHTML = "Tu mensaje cifrado es: " + cipher.decode(Number(offset.value),string.value);
+            
+        });
+
+
+        }else if(getCipherText2 === null){
+         //sino es porque SÍ estamos en CIFRAR
+                getCipherText.addEventListener("click", function (event){
+                    event.preventDefault(); // Previene recarga del navegador
+                    //alert("esto va bien");
+                    //console.log(document.getElementById("message").value);
+                    //console.log(document.getElementById("offsetID").value);
+                
+                
+                    // alert(cipher.encode(offset, string));
+                    //cipher.encode(offset, string);
+                    //document.getElementById("m").innerHTML = "Tu mensaje cifrado es: " + m;
+                document.getElementById("cipherText").innerHTML = "Tu mensaje cifrado es: " + cipher.encode(Number(offset.value),string.value);
+                    
+        });
+
+
+        }
+
+
+
+
 
 
