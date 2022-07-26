a9374624dbc8181e1c958600142526316bc8f797
Arely Gutiérrez <yleraycnan@hotmail.com>
2022-07-26 12:00:24 -0500

    offset 33, mayores a cero, no negativos

--- src/cipher.js
+++ src/cipher.js
@@ -3,50 +3,45 @@
 const cipher = {
   
      encode: function (offset, string) {
-        //console.log(offset)
-        //console.log(string)
+        
         let m = '';
         let SToN="";
         let j="";
 
-  if (document.getElementById("offsetID").value < 0){
-          alert(" 😱 Por favor introduce un número positivo ➕ 😊")
+  if (document.getElementById("offsetID").value < 0 || document.getElementById("offsetID").value == 0 ){
+          alert(" 😱 Por favor introduce un número positivo mayor a cero ➕ 😊")
+
   } else {
         
         for(let i=0; i<string.length; i++){
-            //console.log(string.charCodeAt(i));
+            
             SToN =string.charCodeAt(i);//string to number
           if(SToN != 32){  
-            //console.log(SToN);
+            
              j = Math.abs(SToN - 65 + offset) % 26 + 65; //Convierte a numero y hago operaciones para cifrado
-             //console.log(Math.abs(SToN))
-             //console.log(j);
              m += String.fromCharCode(j); //devuelve a ASCII
-             //console.log(m)
             }else {
             m += String.fromCharCode(SToN);
             }
-        }
-        //document.getElementById("m").innerHTML = "Tu mensaje cifrado es: " + m;  
+        }  
         return m;
 }
       },
 //////////////////////////////////////////////////////////////////
     decode: function (offset, string) {
-      //console.log(offset)
-      //console.log(string)
     let m = '';
       let SToN="";
       let j="";
 
-  if (document.getElementById("offsetID").value < 0){
-      alert(" 😱 Por favor introduce un número positivo ➕ 😊")
-  } else {
+  if (document.getElementById("offsetID").value < 0 || document.getElementById("offsetID").value == 0 ){
+        alert(" 😱 Por favor introduce un número positivo mayor a cero ➕ 😊")
+
+  //} else if{
+
+  }else{
       
       for(let i=0; i<string.length; i++){
-         //console.log(string.charCodeAt(i));
           SToN =string.charCodeAt(i);//string to number
-          //console.log(SToN);
 
           
               let g = (SToN -65 - offset) 
@@ -63,23 +58,15 @@ const cipher = {
               }else{
 
               j = (SToN -65 - offset) % 26 + 65; //Convierte a numero y hago operaciones para cifrado
-              //console.log(SToN -65 - offset );
-              //console.log(SToN -65 - offset )%26;
-              //console.log(j);
               m += String.fromCharCode(j); //devuelve a ASCII
-              //console.log(m)
-              //console.log(document.getElementById("m").value);*/
               }  
         }
-      }
-      //document.getElementById("m").innerHTML = "Tu mensaje cifrado es: " + m;  
+      }  
       return m;
   }
 }
 };
-/////metodos cipher.enconde y cipher.decode/
-//cipher.encode();
-//cipher.encode();
+
 
 
 
