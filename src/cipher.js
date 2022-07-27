ab1ff7a3cdf5723915a1013f0c41d3bd3bf48305
ana-lopez <anitalopez@192.168.1.9>
2022-07-27 01:36:24 -0500

    cifrado cesar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,59 @@
-const cipher = {
-  // ...
-};
+const cipher = {  // Esta guardando las funciones dentro de un objeto
+// toda la logica
+ encode:function(offset,plainText){
+  let solved = ""
+    if(typeof offset == 'number' && typeof plainText == 'string'){
+    
+    for (let i = 0; i< plainText.length; i++){
+        let asciiNum = plainText[i].charCodeAt();
+        //console.log(asciiNum);
+        if (asciiNum >= 65 && asciiNum <= 90){
+          let codedMessage = (asciiNum - 65 + offset) %26 + 65;
+          //console.log(codedMessage);
+          solved += String.fromCharCode(codedMessage);
+         // console.log("solved",solved);
+        } else if (asciiNum >= 97 && asciiNum <= 122){
+            let codedMessage = (asciiNum - 97 + offset) %26 + 97;
+            //console.log(codedMessage);
+            solved += String.fromCharCode(codedMessage);
+          } 
+    } 
+    return solved; 
+    //console.log(solved); 
+  }else {
+   throw TypeError('wrong argument type');
+  }
+  },
+  decode:function(offset,outputText){
+    if(typeof offset == 'number' && typeof outputText == 'string'){
+    let result = ""
+    for(let i = 0; i< outputText.length; i++){
+      let asciiNum = outputText[i].charCodeAt();
+      //console.log(asciiNum); 
+      if (asciiNum >=65 && asciiNum <=90){
+        let decodedMsg = asciiNum -(offset%26);
+        if (asciiNum < 65){
+          decodedMsg = asciiNum -(offset%26);
+          //console.log(decodedMsg + 'esta imprimiendo esto');
+          //result += String.fromCharCode(decodedMsg);
+        }
+        //console.log(decodedMsg);
+        result += String.fromCharCode(decodedMsg);
+      //  console.log(result);
+      } else if (asciiNum >=97 && asciiNum <=122){
+          let decodedMsg = asciiNum -(offset%26)
+         // console.log(decodedMsg);
+          result += String.fromCharCode(decodedMsg);
+      }
+    }
+    return result;
+  //return unsolved;
+  
+  }else {
+  throw TypeError('wrong argument type')
+}
+}
+}
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,50 @@
 import cipher from './cipher.js';
+// todo lo que ver con manipulacion del DOM
+let btnCifrar = document.getElementById('cifrar');
+btnCifrar.addEventListener('click', function(){
+    let plainText = document.getElementById('plainText').value;
+    let offset = parseInt(document.getElementById('offset').value); // parseInt() convierte un string a un entero
+     //manda a llamar a la funcion dentro del objeto, y le pasa los valores de offset e input a cipher.js
+    document.getElementById('input2').innerHTML = cipher.encode(offset,plainText);
+    
+});
 
-console.log(cipher);
+//console.log(cipher);
 
-function newPage(){
-    console.log("hola");
-}
+const btnDecode = document.getElementById('btnDecode');
+btnDecode.addEventListener('click',function(){
+    let outputText = document.getElementById('input2').value;
+    let offset = parseInt(document.getElementById('offset').value); // parseInt() convierte un string a un entero
+    document.getElementById('plainText').innerHTML = cipher.decode(offset,outputText);
+});
+
+
+
+
+
+//Sirve para ocultar estos elementos al cargar el html. 
+//Al no estar dentro de una funcion permite que se escondan estos elementos.
+document.getElementById('container2').style.display ='none';
+document.getElementById('areaTexts').style.display ='none';
+//funcion para mostrar los elementos ocultos
+const boton= document.getElementById('boton');
+boton.addEventListener('click',showScreen);
+function showScreen(){
+    document.getElementById('container2').style.display ='flex';
+    document.getElementById('areaTexts').style.display ='flex';
+    document.getElementById('hi').style.display ='none';
+    document.getElementById('centrar').style.display ='none';
+    document.getElementById('boton').style.display ='none';
+   // document.getElementsByTagName('p').style.display = 'none';
+ //   document.getElementsById('offset').style.display = 'none';
+    }
+
+    //Funcion para borrar las areas de texto
+   let btnClear = document.getElementById('btnClear');
+   btnClear.addEventListener('click',clearText)
+   function clearText(){
+       document.getElementById('plainText').value = '';
+       document.getElementById('input2').value = '';
+       document.getElementById('offset').value = '';
+   }
+
